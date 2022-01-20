57efed66d0dc5e643a5921e99a8a3db9a7354753
qpalzmm22 <qpalzmm22@gmail.com>
2022-01-20 11:12:28 +0900

    fixed makefile

--- Makefile
+++ Makefile
@@ -7,8 +7,5 @@ all : mjyint
 mjyint : 
 	gcc -o mjyint mjyint.c ${debug}
 
-fuzz : 
-	gcc -o fuzz_mjyint fuzz_mjyint.c
-
 clean :
 	rm -rf mjyint out seeds fuzz 
--- test.sh
+++ test.sh
@@ -2,14 +2,15 @@
 
 test_prog="./mjyint "
 
-declare -a tclist=("{+ 1 2}"
-  "{with {x {with {t 3} {+ t 1}}} {+ 3 4}}"
-  "{+ {- {+ 2 2} {+ {- 2 4} 4}}}"
+declare -a tclist=(
   "hel"
+  "{the 3}"
+  "{+ 1 2}"
+  "{+ {- {+ 2 2} {+ {- 2 4} 4}}}"
+  "{with {x {with {t 3} {+ t 1}}} {+ 3 4}}"
   "{with {x {with {t x} {+ t 1}}} {+ 3 4}}" # free variable
   "{with {x 2} {+ {with {x 3} {+ x 1}} 4}}" # 7
   "{+ {- {+ 2 2} {+ {with {x 3} {+ 1 x}} 2}} 1}"
-  "{the 3}"
   "{{fun {x} {+ x 1}} 7}"
   "{with {x 3} {with {f {fun {y} {+ x y}}} {with {x 5} {f 4}}}}"
   "{with {f {dsfun {y} {+ x y}}} {with {x 5} {f 4}}}"
